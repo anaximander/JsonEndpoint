10d9a7fa9199d858fbecf260121535e5deb5ea2c
Jonathan McKenzie <jmckenzie@localytics.com>
2017-11-30 16:11:18 -0500

    Removing slug, updating README

--- djangosite/endpoint/models.py
+++ djangosite/endpoint/models.py
@@ -1,14 +1,10 @@
 from django.db import models
 from jsonfield import JSONField
 from taggit.managers import TaggableManager
-from slugify import slugify
-#import editarea
 
 class JsonEndpoint(models.Model):
     name = models.CharField(max_length=255, unique=True)
     blob = JSONField()
-    #blob = editarea.EditAreaField()
-    slug = models.SlugField(max_length=255, unique=True)
 
     tags = TaggableManager(blank=True)
 
@@ -18,28 +14,22 @@ class JsonEndpoint(models.Model):
     def __unicode__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
-        super(JsonEndpoint, self).save(*args, **kwargs)
-
     def url(self):
-        return "/%s/" % self.slug
+        return "/%s/" % self.name
 
 
 class AuthEndpoint(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    slug = models.SlugField(max_length=255, unique=True)
     enabled = models.BooleanField(default=False)
 
     def __unicode__(self):
         return self.name
 
     def save(self, *args, **kwargs):
-        self.slug = slugify(self.name)
         super(AuthEndpoint, self).save(*args, **kwargs)
 
     def url(self):
-        return "/%s/" % self.slug
+        return "/%s/" % self.name
 
 
 class MockObjectClass(models.Model):
