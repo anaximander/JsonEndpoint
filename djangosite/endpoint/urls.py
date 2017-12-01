0c3de55f4b95350ab580a964d025288e39ca73fe
Jonathan McKenzie <jmckenzie@localytics.com>
2017-12-01 10:11:53 -0500

    Accept slash in url

--- djangosite/endpoint/urls.py
+++ djangosite/endpoint/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, include, url
 from .views import IndexView, DetailView, AuthView, MockObjectView
 
 urlpatterns = patterns('',
-	url(r'^(?P<name>[-\w]+)/$', DetailView.as_view(), name='endpoint'),
+	url(r'^(?P<name>[-\w/]+)/$', DetailView.as_view(), name='endpoint'),
     url(r'^$', IndexView.as_view(), name='index'),
 	url(r'^auth/(?P<name>[-\w]+)/$', AuthView.as_view(), name='auth_endpoint'),
     url(r'^mock_objects/(?P<class_name>[_\w]+)/(?P<object_id>[-\w\d]+)/$', MockObjectView.as_view(), name='mock_objects'),
